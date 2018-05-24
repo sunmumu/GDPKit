ae41159b5056b851f4450e6b817cb80c6427e938
sunmumu <335089101@qq.cm>
2018-05-24 13:56:09 +0800

    GDPKit接口调用示例
    GDPKit接口调用示例

--- GDPKit/GDPKit/ViewController.m
+++ GDPKit/GDPKit/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "GDPKit.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
+    UIImage *image = [UIImage imageNamed:@"hua"];
+    imageView.image = image;
+    [self.view addSubview:imageView];
+    
+    NSString *version = [NSString appVersion];
+    NSLog(@"app的版本号是%@", version);
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
